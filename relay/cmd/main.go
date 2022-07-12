e739347e5f38d39f4c6c6f01089d2303901b5d51
notedit <notedit@gmail.com>
2022-07-12 22:47:53 +0800

    fix remove daemon

--- relay/cmd/main.go
+++ relay/cmd/main.go
@@ -4,30 +4,11 @@ import (
 	"log"
 
 	"github.com/notedit/relay-server/relay"
-	"github.com/sevlyar/go-daemon"
 )
 
 var configFile = "../../config.yaml"
 
 func main() {
-
-	cntxt := &daemon.Context{
-		PidFileName: "relayserver.pid",
-		PidFilePerm: 0644,
-		LogFileName: "relayserver.log",
-		LogFilePerm: 0640,
-		WorkDir:     "./",
-		Umask:       027,
-	}
-
-	d, err := cntxt.Reborn()
-	if err != nil {
-		log.Fatal("Unable to run: ", err)
-	}
-	if d != nil {
-		return
-	}
-	defer cntxt.Release()
 
 	log.Print("- - - - - - - - - - - - - - -")
 	log.Print("daemon started")
